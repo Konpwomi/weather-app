e367b4fe6b5c2304a4d109488661aa915d9b4db2
Konpwomi <patiphan.arp@gmail.com>
2025-02-13 00:44:31 +0700

    feat: display data following the city

--- src/components/weatherCard.tsx
+++ src/components/weatherCard.tsx
@@ -1,25 +1,76 @@
-import React from 'react';
-import { ReactSVG } from 'react-svg';
 import searchIcon from "../components/icons/search.svg";
+import clear from "../assets/clear.png";
+// import drizzle from "../assets/drizzle.png";
+import humidity from "../assets/humidity.png";
+import wind from "../assets/wind.png";
+import useWeather from "../hooks/useWeather";
+import { useState } from "react";
 
-function weatherCard() {
+function WeatherCard() {
+  const [city, setCity] = useState<string>("");
+  const [searchCity, setSearchCity] = useState<string>("Bangkok");
+  const { weather, loading, error } = useWeather(searchCity);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setCity(event.target.value);
+  };
+
+  const handleSearch = () => {
+    if (city.trim() === "") {
+        alert("Please enter a valid city name.");
+        return;
+      }
+    setSearchCity(city);
+  };
+
+  const temperatureCelsius = weather.main ? (weather.main.temp - 273.15).toFixed(2) : null;
+
+
   return (
-    <div className="min-h-screen flex items-center justify-center">
+    <div className="min-h-screen font-poppins flex items-center bg-blue-50 justify-center">
       <div className="bg-gradient-to-b from-blue-400 to-blue-700 p-6 rounded-lg h-screen max-h-[500px] w-[360px]">
-        <div className="flex flex-col">
-          <div className="flex justify-between mx-5">
+        <div className="flex flex-col items-center">
+          <div className="flex gap-5">
             <input
               placeholder="search"
-              className="bg-white rounded-full p-3"
+              className="bg-white rounded-full pl-5 p-3"
+              onChange={handleChange}
             ></input>
-            <button className="rounded-full p-3 bg-white">
-              <ReactSVG className='' src={searchIcon}/>
+            <button className="rounded-full p-4 bg-white" onClick={handleSearch}>
+              <img src={searchIcon} className="w-4 h-4"></img>
             </button>
           </div>
+          {loading ? (
+            <p className="text-white">Loading...</p>
+          ) : error ? (
+            <p className="text-red-500">{error}</p>
+          ) : (
+            <>
+              <img src={clear} className="w-[160px] h-[160px] my-4"></img>
+              <p className="text-6xl text-white">{temperatureCelsius}°c</p>
+              <p className="text-3xl mb-16 text-white">{weather?.name}</p>
+              <div className="flex justify-center gap-18">
+                <div className="flex items-center text-white gap-3">
+                  <img src={humidity} className="w-6 h-6"></img>
+                  <div className="flex flex-col items-center">
+                    <p className="text-lg">{weather.main?.humidity} %</p>
+                    <p className="text-sm">Humidity</p>
+                  </div>
+                </div>
+                <div className="flex items-center text-white gap-3">
+                  <img src={wind} className="w-7 h-7"></img>
+                  <div className="flex flex-col items-center">
+                    <p className="text-lg">{weather.wind?.speed} Km/h</p>
+                    <p className="text-sm">Wind Speed</p>
+                  </div>
+                </div>
+              </div>
+            </>
+          )}
         </div>
       </div>
     </div>
   );
 }
 
-export default weatherCard;
+export default WeatherCard;
---
+++ src/hooks/useWeather.ts
@@ -0,0 +1,40 @@
+import axios from "axios"
+import { useState, useEffect } from "react"
+import { weatherType } from "../types/weatherType"
+
+// 646bbd85409c12c97172c68241ec6885
+// city:string
+
+const useWeather = (city: string): { weather: weatherType; loading: boolean; error: string | null} => {
+    const [weather, setWeather] = useState<weatherType>({} as weatherType);
+    const [loading, setLoading] = useState<boolean>(false);
+    const [error, setError] = useState<string | null>(null);
+
+    useEffect (()=> {
+        const fetchWeather = async () => {
+            try {
+                setLoading(true)
+                setError(null)
+                const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=646bbd85409c12c97172c68241ec6885`)
+                setWeather(response.data)
+            } catch (err) {
+                if (axios.isAxiosError(err) && err.response?.status === 404) {
+                    setError("City not found");
+                  } else {
+                    setError("Failed to fetch weather data");
+                  }
+            } finally {
+                setLoading(false)
+            }
+        }
+        if (city) {
+            fetchWeather()
+        }
+    },[city])
+
+    return { weather, loading, error}
+}
+
+export default useWeather
+
+
