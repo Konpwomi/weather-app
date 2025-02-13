633f835f86c444e4f0bf810c2d43044fefe44758
Konpwomi <patiphan.arp@gmail.com>
2025-02-13 10:35:07 +0700

    fix: layout 5

--- src/components/weatherCard.tsx
+++ src/components/weatherCard.tsx
@@ -28,49 +28,51 @@ function WeatherCard() {
     : null;
 
   return (
-    <div className="min-h-screen font-poppins flex items-center bg-blue-50 justify-center">
-      <div className="bg-gradient-to-b from-blue-400 to-blue-700 p-6 rounded-lg">
-        <div className="flex flex-col items-center">
-          <div className="flex gap-5">
-            <input
-              placeholder="search"
-              className="bg-white rounded-full pl-5 p-3"
-              onChange={handleChange}
-            ></input>
-            <button
-              className="rounded-full p-4 bg-white"
-              onClick={handleSearch}
-            >
-              <img src={searchIcon} className="w-4 h-4"></img>
-            </button>
+    <div className=" flex items-center justify-center h-screen">
+      <div className="font-poppins bg-blue-50 ">
+        <div className="bg-gradient-to-b from-blue-400 to-blue-700 p-6 rounded-lg">
+          <div className="flex flex-col items-center">
+            <div className="flex gap-5">
+              <input
+                placeholder="search"
+                className="bg-white rounded-full pl-5 p-3"
+                onChange={handleChange}
+              ></input>
+              <button
+                className="rounded-full p-4 bg-white"
+                onClick={handleSearch}
+              >
+                <img src={searchIcon} className="w-4 h-4"></img>
+              </button>
+            </div>
+            {loading ? (
+              <p className="text-white">Loading...</p>
+            ) : error ? (
+              <p className="text-red-500">{error}</p>
+            ) : (
+              <>
+                <img src={clear} className="w-[160px] h-[160px] my-4"></img>
+                <p className="text-6xl text-white">{temperatureCelsius}°c</p>
+                <p className="text-3xl mb-16 text-white">{weather?.name}</p>
+                <div className="flex justify-center gap-18">
+                  <div className="flex items-center text-white gap-3">
+                    <img src={humidity} className="w-6 h-6"></img>
+                    <div className="flex flex-col items-center">
+                      <p className="text-lg">{weather.main?.humidity} %</p>
+                      <p className="text-sm">Humidity</p>
+                    </div>
+                  </div>
+                  <div className="flex items-center text-white gap-3">
+                    <img src={wind} className="w-7 h-7"></img>
+                    <div className="flex flex-col items-center">
+                      <p className="text-lg">{weather.wind?.speed} Km/h</p>
+                      <p className="text-sm">Wind Speed</p>
+                    </div>
+                  </div>
+                </div>
+              </>
+            )}
           </div>
-          {loading ? (
-            <p className="text-white">Loading...</p>
-          ) : error ? (
-            <p className="text-red-500">{error}</p>
-          ) : (
-            <>
-              <img src={clear} className="w-[160px] h-[160px] my-4"></img>
-              <p className="text-6xl text-white">{temperatureCelsius}°c</p>
-              <p className="text-3xl mb-16 text-white">{weather?.name}</p>
-              <div className="flex justify-center gap-18">
-                <div className="flex items-center text-white gap-3">
-                  <img src={humidity} className="w-6 h-6"></img>
-                  <div className="flex flex-col items-center">
-                    <p className="text-lg">{weather.main?.humidity} %</p>
-                    <p className="text-sm">Humidity</p>
-                  </div>
-                </div>
-                <div className="flex items-center text-white gap-3">
-                  <img src={wind} className="w-7 h-7"></img>
-                  <div className="flex flex-col items-center">
-                    <p className="text-lg">{weather.wind?.speed} Km/h</p>
-                    <p className="text-sm">Wind Speed</p>
-                  </div>
-                </div>
-              </div>
-            </>
-          )}
         </div>
       </div>
     </div>
